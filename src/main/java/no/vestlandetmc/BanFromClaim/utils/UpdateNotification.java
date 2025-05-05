650b91f5fc39768d31d104bf60ed75d01a013cf6
Lars Martinsen <martinsen@hotmail.com>
2025-05-05 03:50:18 +0200

    Changed to Modrinth version check

--- src/main/java/no/vestlandetmc/BanFromClaim/utils/UpdateNotification.java
+++ src/main/java/no/vestlandetmc/BanFromClaim/utils/UpdateNotification.java
@@ -1,56 +1,71 @@
 package no.vestlandetmc.BanFromClaim.utils;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.Getter;
 import no.vestlandetmc.BanFromClaim.BfcPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.URLConnection;
 
+@SuppressWarnings("UnstableApiUsage")
 public abstract class UpdateNotification extends BukkitRunnable {
 
 	@Getter
-	private static int projectId;
+	private static String projectSlug;
 	@Getter
 	private static String latestVersion = "";
 
-	public UpdateNotification(int projectId) {
-		UpdateNotification.projectId = projectId;
+	public UpdateNotification(String slug) {
+		UpdateNotification.projectSlug = slug;
 	}
 
 	@Override
 	public void run() {
-
 		try {
-			final URI uri = new URI("https://api.spigotmc.org/legacy/update.php?resource=" + projectId);
-			final URL url = uri.toURL();
-			final URLConnection con = url.openConnection();
+			URI uri = new URI("https://api.modrinth.com/v2/project/" + projectSlug + "/version");
+			URLConnection con = uri.toURL().openConnection();
+			con.setRequestProperty("User-Agent", "YourPluginName/1.0");
 
-			try (BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-				latestVersion = r.readLine();
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+				JsonElement element = JsonParser.parseReader(reader);
+
+				if (!element.isJsonArray()) return;
+
+				JsonArray array = element.getAsJsonArray();
+				for (JsonElement e : array) {
+					JsonObject version = e.getAsJsonObject();
+
+					if (!version.get("version_type").getAsString().equalsIgnoreCase("release")) {
+						continue;
+					}
+
+					latestVersion = version.get("version_number").getAsString();
+					break;
+				}
 			}
 
 			if (isUpdateAvailable()) {
 				onUpdateAvailable();
 			}
 
-		} catch (final IOException | URISyntaxException e) {
-			e.getStackTrace();
+		} catch (Exception e) {
+			BfcPlugin.getPlugin().getLogger().severe(e.getMessage());
 		}
 	}
 
 	public abstract void onUpdateAvailable();
 
 	public static boolean isUpdateAvailable() {
-		return !latestVersion.equals(BfcPlugin.getPlugin().getDescription().getVersion());
+		return !latestVersion.equals(BfcPlugin.getPlugin().getPluginMeta().getVersion());
 	}
 
 	public static String getCurrentVersion() {
-		return BfcPlugin.getPlugin().getDescription().getVersion();
+		return BfcPlugin.getPlugin().getPluginMeta().getVersion();
 	}
 }
